58c2358120b22d4f5f3e7889e3b1a8d56c7b1c4c
Cristina-esquej-p <Cristinaesquej@gmail.com>
2019-12-26 18:01:00 +0100

    2act

---
+++ js/functions.js
@@ -0,0 +1,118 @@
+var time = new Date();
+var color;
+var myElement;
+if (time==12){ 
+    color = 'red';
+}
+else if(time==13) {
+    color = 'blue';
+}
+else {
+    color ='yellow';
+}
+myElement=document.getElementById('timecolor');
+myElement.style.backgroundColor = color;
+
+
+<div>
+
+        
+<li>iphone 8</li>
+<li><img src="img/iphone 8.png"width="20%"></li>
+<li>iphone 8 Plus</li>
+<li><img src="img/iphone 8 plus.png"width="15%"></li>
+
+<li>iphone XR</li>
+<a href="https://www.apple.com/es/shop/buy-iphone/iphone-xr/pantalla-de-6,1%E2%80%B3-64gb-azul">enlace a iphone X azul</a>
+<li>iphone 11</li>
+<a href="https://www.apple.com/es/shop/buy-iphone/iphone-11">enlace a iphone 11 verde</a>
+<li>iphone 11 PRO</li> 
+<a href="https://www.apple.com/es/shop/buy-iphone/iphone-11-pro">enlace a iphone 11 PRO rosa</a>       
+</ul>
+</div>
+
+<div class="container">
+        <div class="row">
+            
+        </div>
+        
+          <div class="card" style="width: 18rem;">
+            <img src="img/iphone 8.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">iPhone 8</h5>
+              <p class="card-text">Retina HD
+                Pantalla panorámica LCD Multi‑Touch de 4,7 pulgadas (en diagonal) con tecnología IPS
+                Resolución de 1.334 por 750 píxeles a 326 p/p
+                Contraste de 1.400:1 (típico)</p>
+              <a href="https://www.apple.com/es/shop/buy-iphone/iphone-8" class="btn btn-primary">Buy</a>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/iphone 8 plus.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">iPhone 8 Plus</h5>
+                  <p class="card-text">Retina HD
+                    Pantalla panorámica LCD Multi‑Touch de 5,5 pulgadas (en diagonal) con tecnología IPS
+                    Resolución de 1.920 por 1.080 píxeles a 401 p/p
+                    Contraste de 1.300:1 (típico)</p>
+                  <a href="https://www.apple.com/es/shop/buy-iphone/iphone-8" class="btn btn-primary">Buy</a>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="img/iphone-xr.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">iPhone XR</h5>
+                  <p class="card-text">Liquid Retina HD
+                    Pantalla LCD Multi-Touch de 6,1 pulgadas (en diagonal) con tecnología IPS
+                    Resolución de 1.792 por 828 píxeles a 326 p/p
+                    Contraste de 1.400:1 (típico)
+                    Pantalla True Tone
+                    Gama cromática amplia (P3)
+                    Respuesta háptica
+                    Brillo máximo de 625 nits (típico)</p>
+                  <a href="https://www.apple.com/es/shop/buy-iphone/iphone-xr/pantalla-de-6,1%E2%80%B3-128gb-rojo" class="btn btn-primary">Buy</a>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="img/iphone11-.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">iPhone 11</h5>
+                  <p class="card-text">Liquid Retina HD
+                    Pantalla LCD Multi-Touch de 6,1 pulgadas (en diagonal) con tecnología IPS
+                    Resolución de 1.792 por 828 píxeles a 326 p/p
+                    Contraste de 1.400:1 (típico)
+                    Pantalla True Tone
+                    Gama cromática amplia (P3)
+                    Respuesta háptica
+                    Brillo máximo de 625 nits (típico)
+                    Cubierta oleófuga antihuellas</p>
+                  <a href="https://www.apple.com/es/shop/buy-iphone/iphone-11" class="btn btn-primary">Buy</a>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="img/iphone-11-pro-gold-select-2019_GEO_EMEA.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">iPhone 11 PRO</h5>
+                  <p class="card-text">uper Retina XDR
+                      Pantalla OLED Multi‑Touch de 5,8 pulgadas (en diagonal)
+                      Pantalla HDR
+                      Resolución de 2.436 por 1.125 píxeles a 458 p/p
+                      Contraste de 2.000.000:1 (típico)</p>
+                  <a href="https://www.apple.com/es/shop/buy-iphone/iphone-11-pro" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="img/apple-iphone-11-pro-max-.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">iPhone 11 PRO max</h5>
+                  <p class="card-text">Super Retina XDR
+                      Pantalla OLED Multi‑Touch de 6,5 pulgadas (en diagonal)
+                      Pantalla HDR
+                      Resolución de 2.688 por 1.242 píxeles a 458 p/p
+                      Contraste de 2.000.000:1 (típico)</p>
+                  <a href="https://www.apple.com/es/shop/buy-iphone/iphone-11-pro" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+          
+              </div>
+          </div>
+    </div>
